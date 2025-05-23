cf91b9f1ad1a22ffbb1c91092df53d2f679a1218
DevPBDias <devpbdias@gmail.com>
2025-05-23 15:59:53 -0300

    feat: add question 6

--- 06-operadores-matematicos.py
+++ 06-operadores-matematicos.py
@@ -7,3 +7,14 @@ print(a + b, a - b, a * b, a / b, a ** b, a // b, a % b)
 Crie quatro novas variaveis de numeros inteiros, some elas, 
 calcule a média e exiba o resultado na tela
 """
+
+a = 10
+b = 20
+c = 30
+d = 40
+soma = a + b + c + d
+media = soma / 4
+
+print("A soma é:", soma)
+print("A média é:", media)
+
