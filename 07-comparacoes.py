3c1ba0af70628850a8c77d861d5a1423199eea5e
DevPBDias <devpbdias@gmail.com>
2025-05-23 15:59:58 -0300

    feat: add question 7

--- 07-comparacoes.py
+++ 07-comparacoes.py
@@ -3,3 +3,13 @@ y = 15
 print(x > y, x < y, x == y, x != y)
 
 #Compare dois números e exiba uma frase na tela de acordo com a comparação escolhida
+
+x = 23
+y = 24
+
+comparacao = x > y
+
+if comparacao:
+    print("O x é maior que o y")
+else:
+    print("O x não é maior que o y")
