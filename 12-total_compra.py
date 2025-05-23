60cf932519ea2394e0a9eeabe0325e17e5c80011
DevPBDias <devpbdias@gmail.com>
2025-05-23 16:00:32 -0300

    feat: add extra question 12

---
+++ 12-total_compra.py
@@ -0,0 +1,9 @@
+nome_produto = "Cerveja"
+qtde_produto = 12
+preco_produto = 3.59
+
+valor_total = qtde_produto * preco_produto
+
+print(f"O valor total da compra de {qtde_produto} {nome_produto}s = {valor_total:.2f}")
+
+# Desenhar no papel o fluxo do usuario e oq mexer no app e descrever as conexao com aws a cada movimento do usuario - fluxograma de funcionamento do app dps as funções
